5030dfa035266408fcebbb3d1c3087424e293e41
Gary Gordhamer <ggordham@gmail.com>
2022-01-06 10:48:31 -0600

    clean up login.sql issues

--- splus.sh
+++ splus.sh
@@ -80,14 +80,15 @@ export LD_LIBRARY_PATH="$ORACLE_HOME"/lib:/usr/lib
 
 # Create a SQL login profile, this will define 
 #  a PDB connect string for us to use in lab scripts
-echo "set pagesize 999" > "$working_dir"/login.sql
-echo "set linesize 200" > "$working_dir"/login.sql
-echo "ALTER SESSION SET nls_date_format = 'HH:MI:SS';" > "$working_dir"/login.sql
-echo 'SET SQLPROMPT "_USER'@'_CONNECT_IDENTIFIER _DATE> "' > "$working_dir"/login.sql
+echo "" > "$working_dir"/login.sql 
+echo "set pagesize 999" >> "$working_dir"/login.sql
+echo "set linesize 200" >> "$working_dir"/login.sql
+echo "ALTER SESSION SET nls_date_format = 'HH:MI:SS';" >> "$working_dir"/login.sql
+echo 'SET SQLPROMPT "_USER'@'_CONNECT_IDENTIFIER _DATE> "' >> "$working_dir"/login.sql
 if [ "$ORACLE_PDB_SID" == "" ]; then
-  echo "define con_pdb =''"  > "$working_dir"/login.sql
+  echo "define con_pdb =''"  >> "$working_dir"/login.sql
 else
-  echo "define con_pdb ='@$ORACLE_PDB_SID'"  > "$working_dir"/login.sql
+  echo "define con_pdb ='@$ORACLE_PDB_SID'"  >> "$working_dir"/login.sql
 fi
 
 sqlplus /nolog
