04933ce524ba4c45c7b7ecf5fd11e646509cd95b
Gary Gordhamer <ggordham@gmail.com>
2022-02-07 10:37:30 -0600

    add show histogram script to spm lab

---
+++ perflab-ECO/spm/show-hist.sql
@@ -0,0 +1,27 @@
+
+-- shows histogram details for
+--   table t1 column d first 5 buckets
+set echo off
+
+connect perflab/perf$lab&con_pdb
+
+PROMPT
+PROMPT Basic table details
+PROMPT
+column table_name format a12
+
+SELECT table_name, num_rows
+  FROM user_tables
+  WHERE table_name IN ('T1','T2');
+
+PROMPT
+PROMPT Histogram information for first 5 buckets
+PROMPT
+column column_name format a12
+
+SELECT column_name, endpoint_number, endpoint_value, endpoint_repeat_count
+  FROM user_tab_histograms
+  WHERE table_name = 'T1' AND column_name = 'D' AND rownum < 6
+  ORDER BY endpoint_number ASC;
+
+ set echo on 
