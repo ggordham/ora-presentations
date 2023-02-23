88688f1145bcba842ba05a276cf02977bcd949b5
Gary Gordhamer <ggordham@gmail.com>
2023-02-23 13:28:33 -0600

    Added signiture to SPM queries

--- spm-UTOUG/list.sql
+++ spm-UTOUG/list.sql
@@ -10,10 +10,11 @@ column plan_name format a32
 column origin format a15
 column last_executed format a30
 column sql_text format a65
+column signature format 99999999999999999999999
 
 set echo on
 
-SELECT sql_handle, plan_name, origin, enabled, accepted,
+SELECT signature, sql_handle, plan_name, origin, enabled, accepted,
        fixed, optimizer_cost, executions, elapsed_time,
        cpu_time, buffer_gets, disk_reads
   FROM dba_sql_plan_baselines;
