dd0eada353d21096c2dac756e8d5da3a9dd4d5d2
Gary Gordhamer <ggordham@gmail.com>
2023-10-18 08:31:03 -0500

    resolving issue with unpacking baselines

--- tune-that-SQL-MKE/unpack-spm.sql
+++ tune-that-SQL-MKE/unpack-spm.sql
@@ -1,10 +1,12 @@
-/* unpack_spm.sql */
+/* unpack-spm.sql */
 
 -- pack up all current baselines
 VARIABLE v_num NUMBER 
 
 BEGIN
- :v_num := DBMS_SPM.UNPACK_STGTAB_BASELINE('SPM_STGTAB', 'PERFLAB');
+ :v_num := DBMS_SPM.UNPACK_STGTAB_BASELINE(table_name => 'SPM_STGTAB', 
+                                           table_owner => 'PERFLAB', 
+                                           creator => 'PERFLAB' );
 END;
 /
 
