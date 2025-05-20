25c6e58c52319a6fccdaaa6325f9dbcc6d7e6a42
Gary Gordhamer <ggordham@gmail.com>
2025-05-20 16:16:37 -0500

    fixes for day 2 lab

---
+++ inside-the-optimizer-ORAPUB/db_build/fixpdb3.sql
@@ -0,0 +1,10 @@
+
+
+GRANT SELECT ON dba_sql_plan_directives TO perflab;
+GRANT SELECT ON dba_sql_plan_dir_objects TO perflab;
+
+GRANT SELECT ON sh.customers TO perflab;
+GRANT SELECT ON oe.orders TO perflab;
+GRANT SELECT ON oe.product_information TO perflab;
+GRANT SELECT ON oe.order_items TO perflab;
+
