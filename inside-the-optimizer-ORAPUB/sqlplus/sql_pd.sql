ce0ed697bab175955e5e96b37290535c1f6f3d72
Gary Gordhamer <ggordham@gmail.com>
2025-05-21 09:40:24 -0500

    fixed typo in spd script

--- inside-the-optimizer-ORAPUB/sqlplus/sql_pd.sql
+++ inside-the-optimizer-ORAPUB/sqlplus/sql_pd.sql
@@ -14,7 +14,7 @@ select pd.directive_id, pd.reason, pd.state, pdo.object_name, pdo.subobject_name
     TO_CHAR (pd. last_used, 'YYYY-MM-DD') As last_used_day, pd.notes, pdo.notes
   from dba_sql_plan_directives pd, dba_sql_plan_dir_objects pdo
   where pd.directive_id = pdo.directive_id
-     and pdo.owner = 'SH';
-     and pdo.object_name = ' CUSTOMERS';
+     and pdo.owner = 'SH'
+     and pdo.object_name = 'CUSTOMERS';
      -- and existsNode (pd.notes,'//text () [contains(.,"&1")]') = 1;
 
